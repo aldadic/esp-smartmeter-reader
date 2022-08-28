ade43beeacce6dbfd8faa265bb6060f7a029924c
aldadic <aleksandar.dadic@outlook.com>
2022-08-28 17:24:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# ESP32 Smart Meter Reader for Wiener Netze
+
+This Arduino sketch was made to read and decrypt the data from my Smart Meter (Landis+Gyr E450 operated by Wiener Netze) and publish it via MQTT. This way the data can be easily integrated into Home Assistant (see [here](#home-assistant-integration)). Although this sketch was made for the ESP32, it should also work with the ESP8266 after minor adjustments (more on that later). This project was only possible with the information from [this thread](https://www.lteforum.at/mobilfunk/wiener-netze-smart-meter-auslesen.16222/); thank you to everyone who shared their findings (especially the user "pocki").
+
+## Setting up the ESP32
+
+To be able to use the optical user interface of the Smart Meter an [IR reader with TTL-Interface](https://wiki.volkszaehler.org/hardware/controllers/ir-schreib-lesekopf-ttl-ausgang) is needed (I am using [this](https://www.ebay.de/itm/313460034498) one). This device reads the data from the optical user interface and transmits it to the ESP32 via serial communication. Since I wanted to use UART2 for this, my IR reader is connected to my ESP32 in the following way:
+
+| IR reader  | ESP32        |
+| ---------- | ------------ |
+| VCC        | 3.3V         |
+| GND        | GND          |
+| RX         | GPIO16 (RX2) |
+| TX         | GPIO17 (TX2) |
+
+If you want to use a different serial port or an ESP8266 instead of the ESP32 (the ESP8266 has only UART0 available), you will need to change the wiring and the serial port that is used by the sketch. This is done in the `config.h` file (see [here](#configuring-and-uploading-the-sketch)).
+
+## Configuring and uploading the sketch
+
+After you cloned the repository, rename the `config_example.h` file to `config.h` and open it. Adjust the settings and save the file (the decryption key can be obtained in the [Smart Meter Webportal](https://smartmeter-web.wienernetze.at/)). After that open `esp32-smartmeter-reader.ino` in the Arduino IDE. The sketch uses the following libaries:
+
+* [mbedtls](https://github.com/Mbed-TLS/mbedtls)
+* [FastCRC](https://github.com/FrankBoesing/FastCRC)
+* [ArduinoJson](https://github.com/bblanchon/ArduinoJson)
+* [PubSubClient](https://github.com/knolleary/pubsubclient)
+
+mbedtls should be already available in the Arduino IDE. All other libraries must be installed via the Library Manager. Once that is done, you should be able to upload the sketch via the Arduino IDE.
+
+## Home Assistant integration
+
+The example below shows how the data can be integrated into Home Assistant:
+
+```yaml
+mqtt:
+  sensor:
+    - name: "Zählerstand"
+      state_topic: "homeassistant/sensor/smartmeter/state"  # use topic from config.h
+      device_class: "energy"
+      unit_of_measurement: "kWh"
+      state_class: "total_increasing"
+      value_template: "{{ value_json['+A'] }}"
+    - name: "Momentanleistung"
+      state_topic: "homeassistant/sensor/smartmeter/state" # use topic from config.h
+      device_class: "power"
+      unit_of_measurement: "W"
+      state_class: "measurement"
+      value_template: "{{ value_json['+P'] }}"
+
+# the history of instantaneous power is not interesting 
+# therefore exclude it from the recorder
+recorder:
+  exclude:
+    entities:
+      - sensor.momentanleistung
+```
---
+++ esp32-smartmeter-reader/esp32-smartmeter-reader.ino
@@ -0,0 +1,186 @@
+#include "config.h"
+#include "mbedtls/aes.h"
+#include <FastCRC.h>
+#include <ArduinoJson.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// --------------- WIFI ---------------
+
+void SetupWifi() {
+  delay(10);
+  console->println();
+  console->print("Connecting to ");
+  console->println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    console->print(".");
+  }
+
+  console->println("");
+  console->println("WiFi connected");
+  console->println("IP address: ");
+  console->println(WiFi.localIP());
+}
+
+// --------------- MQTT ---------------
+
+WiFiClient wifi_client;
+PubSubClient mqtt_client(wifi_client);
+
+void ReconnectMQTT() {
+  while (!mqtt_client.connected()) {
+    console->print("Attempting MQTT connection...");
+    if (mqtt_client.connect("ESPClient", MQTT_USER, MQTT_PASS)) {
+      console->println("connected");
+    } else {
+      console->print("failed, rc=");
+      console->print(mqtt_client.state());
+      console->println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+// --------------- SERIAL READER ---------------
+
+const unsigned int MESSAGE_LENGTH = 105;
+byte received_data[MESSAGE_LENGTH];
+
+void ReadSerialData() {
+  static byte previous_byte = 0;
+  static bool receiving = false;
+  static unsigned int pos;
+
+  while (smart_meter->available() > 0) {
+    byte current_byte = smart_meter->read();
+    if (!receiving) {
+      // Starting sequence is 7E A0
+      if (previous_byte == 0x7E && current_byte == 0xA0) {
+        receiving = true;
+        received_data[0] = 0x7E;
+        received_data[1] = 0xA0;
+        pos = 2;
+      } 
+    } else {
+      if (pos < MESSAGE_LENGTH) {
+        received_data[pos] = current_byte;
+        pos++;
+      } else {
+        ParseReceivedData();
+        receiving = false;
+      }
+    }
+    previous_byte = current_byte;
+  }
+}
+
+// -------------- DATA PARSER ---------------
+
+int BytesToInt(byte bytes[], unsigned int left, unsigned int right) {
+  int result = 0;
+  for (unsigned int i = left; i < right; i++) {
+    result = result * 256 + bytes[i];
+  }
+  return result;
+}
+
+void ParseReceivedData() {
+  // Discard message if CRC check fails
+  if (!ValidateCRC()) {
+    return;
+  }
+
+  // Decrypt message
+  byte decrpyted_message[74];
+  DecryptMessage(decrpyted_message);
+
+  // Extract time and date from decrypted message
+  int year = BytesToInt(decrpyted_message, 22, 24);
+  int month = BytesToInt(decrpyted_message, 24, 25);
+  int day = BytesToInt(decrpyted_message, 25, 26);
+  int hour = BytesToInt(decrpyted_message, 27, 28);
+  int minute = BytesToInt(decrpyted_message, 28, 29);
+  int second = BytesToInt(decrpyted_message, 29, 30);
+  char timestamp[19];
+  sprintf(timestamp, "%02d.%02d.%04d %02d:%02d:%02d", day, month, year, hour, minute, second);
+
+  // Create JSON
+  StaticJsonDocument<256> doc;
+  doc["timestamp"] = timestamp;
+  doc["+A"] = BytesToInt(decrpyted_message, 35, 39)/1000.0;
+  doc["-A"] = BytesToInt(decrpyted_message, 40, 44)/1000.0;
+  doc["+R"] = BytesToInt(decrpyted_message, 45, 49)/1000.0;
+  doc["-R"] = BytesToInt(decrpyted_message, 50, 54)/1000.0;
+  doc["+P"] = BytesToInt(decrpyted_message, 55, 59);
+  doc["-P"] = BytesToInt(decrpyted_message, 60, 64);
+  doc["+Q"] = BytesToInt(decrpyted_message, 65, 69);
+  doc["-Q"] = BytesToInt(decrpyted_message, 70, 74);
+  char payload[256];
+  serializeJson(doc, payload);
+
+  // Publish JSON
+  console->println(payload);
+  if (MQTT_ENABLED) {
+    mqtt_client.publish(MQTT_TOPIC, payload, false);
+  }
+}
+
+// --------------- CRC VALIDATOR ---------------
+
+FastCRC16 CRC16;
+
+bool ValidateCRC() {
+  byte message[101];
+  memcpy(message, received_data + 1, 101);
+  int crc = CRC16.x25(message, 101);
+  int expected_crc = received_data[103] * 256 + received_data[102];
+  if (crc != expected_crc) {
+    console->println("WARNING: CRC check failed");
+    return false;
+  }
+  return true;
+}
+
+// --------------- DECRYPTOR ---------------
+
+mbedtls_aes_context aes;
+
+void DecryptMessage(byte decrpyted_message[74]) {
+  // Extract message and nonce from received data
+  byte encrpyted_message[74] = {0};
+  memcpy(encrpyted_message, received_data + 28, 74);
+  byte nonce[16] = {0};
+  memcpy(nonce, received_data + 14, 8);
+  memcpy(nonce + 8, received_data + 24, 4);
+  nonce[15] = 0x02; 
+
+  // Decrypt message
+  size_t nc_off = 0;
+  unsigned char stream_block[16] = {0};
+  mbedtls_aes_crypt_ctr(&aes, 74, &nc_off, nonce, stream_block, encrpyted_message, decrpyted_message);
+}
+
+// --------------- SETUP & LOOP ---------------
+
+void setup() {
+  console->begin(CONSOLE_BAUD_RATE);
+  smart_meter->begin(SMARTMETER_BAUD_RATE);
+  mbedtls_aes_init(&aes);
+  mbedtls_aes_setkey_enc(&aes, KEY, 128);
+  if (MQTT_ENABLED) {
+    SetupWifi();
+    mqtt_client.setServer(MQTT_SERVER, MQTT_PORT);
+  }
+}
+
+void loop() {
+  if (!mqtt_client.connected()) {
+    ReconnectMQTT();
+  }
+  mqtt_client.loop();
+  ReadSerialData();
+}
